ae1750343817ceeba86163384c95d0be782fab33
Việt <70686146+VietBanna@users.noreply.github.com>
2020-09-26 23:28:20 +0700

    Viet_update_Form
    Form Login and Home

---
+++ QLyHocSinhTHPT/QLyHocSinhTHPT/Form2.Designer.cs
@@ -0,0 +1,286 @@
+﻿namespace QLyHocSinhTHPT
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form2));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lb_control = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btn_Thoat = new System.Windows.Forms.Button();
+            this.btn_Tkb = new System.Windows.Forms.Button();
+            this.btn_Monhoc = new System.Windows.Forms.Button();
+            this.btn_Giaovien = new System.Windows.Forms.Button();
+            this.btn_Lop = new System.Windows.Forms.Button();
+            this.btn_HocSinh = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Image = ((System.Drawing.Image)(resources.GetObject("label1.Image")));
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(1269, 717);
+            this.label1.TabIndex = 0;
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.label4.Location = new System.Drawing.Point(0, 80);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(1270, 10);
+            this.label4.TabIndex = 32;
+            // 
+            // lb_control
+            // 
+            this.lb_control.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.lb_control.Location = new System.Drawing.Point(0, 89);
+            this.lb_control.Name = "lb_control";
+            this.lb_control.Size = new System.Drawing.Size(10, 82);
+            this.lb_control.TabIndex = 31;
+            this.lb_control.Visible = false;
+            // 
+            // label5
+            // 
+            this.label5.BackColor = System.Drawing.Color.ForestGreen;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(-1, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(1270, 80);
+            this.label5.TabIndex = 30;
+            this.label5.Text = "Trường THPT Nguyễn Thiện Thuật";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btn_Thoat
+            // 
+            this.btn_Thoat.BackColor = System.Drawing.Color.ForestGreen;
+            this.btn_Thoat.FlatAppearance.BorderSize = 0;
+            this.btn_Thoat.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Thoat.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Thoat.ForeColor = System.Drawing.Color.White;
+            this.btn_Thoat.Location = new System.Drawing.Point(-6, 530);
+            this.btn_Thoat.Name = "btn_Thoat";
+            this.btn_Thoat.Size = new System.Drawing.Size(194, 82);
+            this.btn_Thoat.TabIndex = 29;
+            this.btn_Thoat.Text = "Thoát";
+            this.btn_Thoat.UseVisualStyleBackColor = false;
+            this.btn_Thoat.Click += new System.EventHandler(this.btn_Thoat_Click);
+            // 
+            // btn_Tkb
+            // 
+            this.btn_Tkb.BackColor = System.Drawing.Color.ForestGreen;
+            this.btn_Tkb.FlatAppearance.BorderSize = 0;
+            this.btn_Tkb.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Tkb.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Tkb.ForeColor = System.Drawing.Color.White;
+            this.btn_Tkb.Location = new System.Drawing.Point(-6, 442);
+            this.btn_Tkb.Name = "btn_Tkb";
+            this.btn_Tkb.Size = new System.Drawing.Size(194, 82);
+            this.btn_Tkb.TabIndex = 28;
+            this.btn_Tkb.Text = "Thời khóa biểu";
+            this.btn_Tkb.UseVisualStyleBackColor = false;
+            this.btn_Tkb.Click += new System.EventHandler(this.btn_Tkb_Click);
+            // 
+            // btn_Monhoc
+            // 
+            this.btn_Monhoc.BackColor = System.Drawing.Color.ForestGreen;
+            this.btn_Monhoc.FlatAppearance.BorderSize = 0;
+            this.btn_Monhoc.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Monhoc.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Monhoc.ForeColor = System.Drawing.Color.White;
+            this.btn_Monhoc.Location = new System.Drawing.Point(-6, 354);
+            this.btn_Monhoc.Name = "btn_Monhoc";
+            this.btn_Monhoc.Size = new System.Drawing.Size(194, 82);
+            this.btn_Monhoc.TabIndex = 27;
+            this.btn_Monhoc.Text = "Môn học";
+            this.btn_Monhoc.UseVisualStyleBackColor = false;
+            this.btn_Monhoc.Click += new System.EventHandler(this.btn_Monhoc_Click);
+            // 
+            // btn_Giaovien
+            // 
+            this.btn_Giaovien.BackColor = System.Drawing.Color.ForestGreen;
+            this.btn_Giaovien.FlatAppearance.BorderSize = 0;
+            this.btn_Giaovien.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Giaovien.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Giaovien.ForeColor = System.Drawing.Color.White;
+            this.btn_Giaovien.Location = new System.Drawing.Point(-6, 178);
+            this.btn_Giaovien.Name = "btn_Giaovien";
+            this.btn_Giaovien.Size = new System.Drawing.Size(194, 82);
+            this.btn_Giaovien.TabIndex = 26;
+            this.btn_Giaovien.Text = "Giáo viên";
+            this.btn_Giaovien.UseVisualStyleBackColor = false;
+            this.btn_Giaovien.Click += new System.EventHandler(this.btn_Giaovien_Click);
+            // 
+            // btn_Lop
+            // 
+            this.btn_Lop.BackColor = System.Drawing.Color.ForestGreen;
+            this.btn_Lop.FlatAppearance.BorderSize = 0;
+            this.btn_Lop.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Lop.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Lop.ForeColor = System.Drawing.Color.White;
+            this.btn_Lop.Location = new System.Drawing.Point(-6, 266);
+            this.btn_Lop.Name = "btn_Lop";
+            this.btn_Lop.Size = new System.Drawing.Size(194, 82);
+            this.btn_Lop.TabIndex = 25;
+            this.btn_Lop.Text = "Lớp";
+            this.btn_Lop.UseVisualStyleBackColor = false;
+            this.btn_Lop.Click += new System.EventHandler(this.btn_Lop_Click);
+            // 
+            // btn_HocSinh
+            // 
+            this.btn_HocSinh.BackColor = System.Drawing.Color.ForestGreen;
+            this.btn_HocSinh.FlatAppearance.BorderSize = 0;
+            this.btn_HocSinh.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_HocSinh.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_HocSinh.ForeColor = System.Drawing.Color.White;
+            this.btn_HocSinh.Location = new System.Drawing.Point(-6, 90);
+            this.btn_HocSinh.Name = "btn_HocSinh";
+            this.btn_HocSinh.Size = new System.Drawing.Size(194, 82);
+            this.btn_HocSinh.TabIndex = 24;
+            this.btn_HocSinh.Text = "Học sinh";
+            this.btn_HocSinh.UseVisualStyleBackColor = false;
+            this.btn_HocSinh.Click += new System.EventHandler(this.btn_HocSinh_Click);
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.label2.Location = new System.Drawing.Point(188, 90);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(10, 630);
+            this.label2.TabIndex = 33;
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.label3.Location = new System.Drawing.Point(-1, 348);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(194, 7);
+            this.label3.TabIndex = 34;
+            // 
+            // label6
+            // 
+            this.label6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.label6.Location = new System.Drawing.Point(-1, 171);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(194, 7);
+            this.label6.TabIndex = 35;
+            // 
+            // label7
+            // 
+            this.label7.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.label7.Location = new System.Drawing.Point(-1, 523);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(194, 7);
+            this.label7.TabIndex = 36;
+            // 
+            // label8
+            // 
+            this.label8.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.label8.Location = new System.Drawing.Point(-1, 436);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(194, 7);
+            this.label8.TabIndex = 37;
+            // 
+            // label9
+            // 
+            this.label9.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.label9.Location = new System.Drawing.Point(-1, 260);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(194, 7);
+            this.label9.TabIndex = 38;
+            // 
+            // label10
+            // 
+            this.label10.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.label10.Location = new System.Drawing.Point(-4, 611);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(194, 119);
+            this.label10.TabIndex = 39;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(1269, 717);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lb_control);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btn_Thoat);
+            this.Controls.Add(this.btn_Tkb);
+            this.Controls.Add(this.btn_Monhoc);
+            this.Controls.Add(this.btn_Giaovien);
+            this.Controls.Add(this.btn_Lop);
+            this.Controls.Add(this.btn_HocSinh);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form2";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form2";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lb_control;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btn_Thoat;
+        private System.Windows.Forms.Button btn_Tkb;
+        private System.Windows.Forms.Button btn_Monhoc;
+        private System.Windows.Forms.Button btn_Giaovien;
+        private System.Windows.Forms.Button btn_Lop;
+        private System.Windows.Forms.Button btn_HocSinh;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+    }
+}
---
+++ QLyHocSinhTHPT/QLyHocSinhTHPT/Form2.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QLyHocSinhTHPT
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_Thoat_Click(object sender, EventArgs e)
+        {
+            this.Dispose();
+        }
+
+        private void btn_HocSinh_Click(object sender, EventArgs e)
+        {
+            lb_control.Visible = true;
+            lb_control.Location = new Point(0, 58);
+        }
+
+        private void btn_Giaovien_Click(object sender, EventArgs e)
+        {
+            lb_control.Visible = true;
+            lb_control.Location = new Point(0, 116);
+        }
+
+        private void btn_Lop_Click(object sender, EventArgs e)
+        {
+            lb_control.Visible = true;
+            lb_control.Location = new Point(0, 174);
+        }
+
+        private void btn_Monhoc_Click(object sender, EventArgs e)
+        {
+            lb_control.Visible = true;
+            lb_control.Location = new Point(0, 232);
+        }
+
+        private void btn_Tkb_Click(object sender, EventArgs e)
+        {
+            lb_control.Visible = true;
+            lb_control.Location = new Point(0, 288);
+        }
+    }
+}
